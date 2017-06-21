175099a26dd28b7e6f7d622ead9bc390cbc56a37
Zhenggan <zhengsmailbox@gmail.com>
2017-06-20 21:54:50 -0700

    Remove support for dining philosopher. Will implement in separate project

--- main.cpp
+++ main.cpp
@@ -6,21 +6,7 @@
 
 int main(int argc, char *argv[])
 {   
-    if (argc != 2)
-    {
-        std::cout  << "Invalid number of arguments, Please enter 0 for mutex experiment and 1 for dining philosopher" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    
-    if (atoi(argv[1]) == 0)
-    {
-        std::cout  << "Running mutex experiment" << std::endl;
-        MutexExperiment();
-    }
-    else if (atoi(argv[1]) == 1)
-    {
-        //TODO:Implement a case of dining philosopher
-        std::cout  << "Dining philosopher not yet implemented. This feature will be added at a later patch :)" << std::endl;
-    }
+    std::cout  << "Running mutex experiment" << std::endl;
+    MutexExperiment();
     return 0;
 }
